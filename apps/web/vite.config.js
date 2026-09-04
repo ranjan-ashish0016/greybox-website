ef7df77efe6810ee3286261d28013720a6060fc3
ranjan-ashish0016 <ashishranjan6206@gmail.com>
2026-09-04 12:13:28 +0530

    Change output directory for web build
    dist

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 	plugins: [react()],
 	build: {
-		outDir: fileURLToPath(new URL('../../dist/apps/web', import.meta.url)),
+		outDir: fileURLToPath(new URL( 'dist', import.meta.url)),
 		emptyOutDir: true,
 	},
 	resolve: {
